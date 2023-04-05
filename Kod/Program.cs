34dc25b541cf79fa25d6c1e5ed680d433dc278f0
IvanMkrv <makarovrabota@mail.ru>
2023-04-05 19:40:30 +0300

    Второй коит, внес часть которая проверяет условеи длинна масива и вносит в коунд второго массива

--- Kod/Program.cs
+++ Kod/Program.cs
@@ -10,4 +10,13 @@ string[] array2 = new string[array1.Length];
         count++;
         }
     }
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
 }
+SecondArrayWithIF(array1, array2);
+PrintArray(array2);
